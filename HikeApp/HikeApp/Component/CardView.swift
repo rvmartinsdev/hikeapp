50b70281a022aef6c9d06298a26c25416d042c05
Rodofo Martins <rodolfo@rvmartins.dev>
2025-07-30 21:00:15 +0100

    WIP

--- HikeApp/HikeApp/Component/CardView.swift
+++ HikeApp/HikeApp/Component/CardView.swift
@@ -13,20 +13,55 @@ struct CardView: View {
         
         ZStack {
             CustomBackgroundView()
-            ZStack {
-                Circle()
-                    .fill(
-                        LinearGradient(
-                            colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")],
-                            startPoint: .topLeading,
-                            endPoint: .bottomTrailing
-                        )
-                    )
-                    .frame(width: 256, height: 256)
+            VStack {
+                //MARK: - HEADER
                 
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                                LinearGradient(
+                                    colors: [.customGrayLight, .customGrayMedium],
+                                    startPoint: .top,
+                                    endPoint: .bottom
+                                )
+                            )
+                        
+                        Button() {
+                            //ACTION: show a sheet
+                            print("The button was pressed")
+                        } label: {
+                            Text("Button")
+                        }
+                    }
+                    Text("Fun and enjoyable acitivies for friends and families.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                }//: HEADER
+                .padding(.horizontal, 30)
+                
+                //MARK: - MAIN CONTENT
+                
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(
+                                colors: [Color.customIndigoMedium, Color.customSalmonLight],
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            )
+                        )
+                        .frame(width: 256, height: 256)
+                    
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                
+                //MARK: - FOOTER
             }
         }//: CARD
         .frame(width: 320, height: 570)
---
+++ HikeApp/HikeApp/Component/CustomButtonView.swift
@@ -0,0 +1,40 @@
+//
+//  CustomButtonView.swift
+//  HikeApp
+//
+//  Created by Rodolfo Vasconcelos on 7/30/25.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(
+                    LinearGradient(
+                        colors: [.white, .customGreenLight, .customGrayMedium],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                )
+            
+            Circle()
+                .stroke(
+                    LinearGradient(
+                        colors: [.customGrayLight, .customGrayMedium],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    ),
+                    lineWidth: 4)
+            
+            
+        }//: ZStack
+        .frame(width: 58, height: 58)
+    }
+}
+
+#Preview {
+    CustomButtonView()
+        .padding()
+}
