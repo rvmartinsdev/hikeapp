ad85160dc3bb40d69e4520f478610e6e30a50c1b
Rodofo Martins <rodolfo@rvmartins.dev>
2025-07-31 14:51:03 +0100

    SwiftUI Labeled Content and SwiftUi links

--- HikeApp/HikeApp/Screen/SettingsView.swift
+++ HikeApp/HikeApp/Screen/SettingsView.swift
@@ -63,6 +63,65 @@ struct SettingsView: View {
             //MARK: - SECTION ICONS
             
             //MARK: - SECTION ABOUT
+            Section (
+                header: Text("ABOUT THE APP"),
+                footer: HStack {
+                    Spacer()
+                    Text("Copyright © All rights reserved")
+                    Spacer()
+                }
+                    .padding(.vertical, 8)
+            ) {
+                CustomListRowView(
+                    rowLabel: "Application",
+                    rowIcon: "apps.iphone",
+                    rowContent: "HikeApp",
+                    rowTintColor: .blue
+                )
+                
+                CustomListRowView(
+                    rowLabel: "Compability",
+                    rowIcon: "info.circle",
+                    rowContent: "iOS,iPadOS",
+                    rowTintColor: .red
+                )
+                
+                CustomListRowView(
+                    rowLabel: "Technology",
+                    rowIcon: "swift",
+                    rowContent: "Swift",
+                    rowTintColor: .orange
+                )
+                
+                CustomListRowView(
+                    rowLabel: "Version",
+                    rowIcon: "gear",
+                    rowContent: "1.0",
+                    rowTintColor: .purple
+                )
+                
+                CustomListRowView(
+                    rowLabel: "Developer",
+                    rowIcon: "ellipsis.curlybraces",
+                    rowContent: "Rodolfo Martins",
+                    rowTintColor: .mint
+                )
+                
+                CustomListRowView(
+                    rowLabel: "Designer",
+                    rowIcon: "paintpalette",
+                    rowContent: "Wilson Donzela",
+                    rowTintColor: .pink
+                )
+                
+                CustomListRowView(
+                    rowLabel: "Website",
+                    rowIcon: "globe",
+                    rowTintColor: .indigo,
+                    rowLinkLabel: "RVMartinsDev",
+                    rowLinkDestination: "https://rvmatins.dev"
+                )
+            }//: SECTION
         }//: LIST
     }
 }
